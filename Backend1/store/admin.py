95d767101882a853ee9b653d934d11d54ce28f37
Adhrit Sigdel <adhritsigdel@gmail.com>
2026-02-11 20:27:04 +0545

    hi

--- Backend1/store/admin.py
+++ Backend1/store/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 # Register your models here.
 
-from .models import Category, Product, UserProfile, Order, OrderItem
+from .models import Category, Product, UserProfile, Order
+from .models import OrderItem, CartItem, Cart, Wishlist
 
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(UserProfile)
 admin.site.register(Order)  
 admin.site.register(OrderItem)  
+admin.site.register(CartItem)
+admin.site.register(Cart)
+admin.site.register(Wishlist)
