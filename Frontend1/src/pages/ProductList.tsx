7d12964b93cfade23ed98b25539cd05016867464
Adhrit Sigdel <adhritsigdel@gmail.com>
2026-04-23 00:06:02 +0545

    _

--- Frontend1/src/pages/ProductList.tsx
+++ Frontend1/src/pages/ProductList.tsx
@@ -11,7 +11,7 @@ function ProductList() {
     const searchQuery = searchParams.get("search");
 
     useEffect(() => {
-        let url = "http://127.0.0.1:8000/api/products/";
+        let url = "https://ecom-store-s6y4.onrender.com/api/products/";
         if (searchQuery) {
             url += `?search=${encodeURIComponent(searchQuery)}`;
         }
