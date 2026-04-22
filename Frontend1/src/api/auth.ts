36a89912e80eac3f0b1c2073679791602e1dade0
Adhrit Sigdel <adhritsigdel@gmail.com>
2026-04-23 00:04:36 +0545

    _

--- Frontend1/src/api/auth.ts
+++ Frontend1/src/api/auth.ts
@@ -1,4 +1,4 @@
-const AUTH_URL = "http://127.0.0.1:8000/api/";
+const AUTH_URL = "https://ecom-store-s6y4.onrender.com/api/";
 
 export const loginUser = async (data: any) => {
     const response = await fetch(`${AUTH_URL}login/`, {
